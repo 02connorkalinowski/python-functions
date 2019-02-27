32257c74f2e2df110610dcb1f12ddc43309c42fd
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-02-27 09:55:59 -0800

    Add Functions and variable, Part 3

--- Functions-Git.py
+++ Functions-Git.py
@@ -23,3 +23,63 @@ def print_something():
 
 print('\r\n', x)
 print_something()
+
+
+
+
+
+# 4.13.6: Functions and variable, Part 3
+# Connor
+#2.18.19
+
+def print_number(x):
+	print('\n', x)
+
+print_number(13)
+print_number(23)
+
+
+# 4.14.4: Name and Age
+# Connor
+# 2.18.19
+
+def name_and_age(name, age):
+	print('Hi, my name is', name, 'and i am', age, 'years old!')
+
+name_and_age('Mike', 33)
+name_and_age('Zane', 18)
+
+# 4.14.5: Default Parameter Values
+# Connor
+# 2.19.19
+
+def print_two_numbers(x, y = 20):
+	print('First number: ', x)
+	print('Second number: ' + str(y))
+
+
+print_two_numbers(34, 45)
+print_two_numbers(78)
+
+
+# 4.14.6: Print sum
+# Connor
+# 2.19.19
+
+def print_sum(x, y):
+	print(x + y)
+
+print_sum(48, 2345)
+
+
+
+# 4.14.7: Print Multiple Times
+# Connor
+# 2.19.19
+
+def print_multiple_times(string, times):
+	for i in range(times):
+		print(string)
+
+
+print_multiple_times('Hey there Computer Scientist' ,10)
