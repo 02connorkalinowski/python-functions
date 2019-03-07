06828022b9e1e09f62169b0859e2adf3a395178e
Connor Kalinowski <02connor.kalinowski@gulllakecs.org>
2019-03-07 12:39:40 -0800

    Add Enter a Number

--- Functions-Git.py
+++ Functions-Git.py
@@ -110,13 +110,13 @@ def print_two_numbers(x, y = 20):
 # 4.16.3: Enter a Number using Try and Except
 # Connor
 # 2.20.19
-#
-# try:
-# 	my_num = int(input('Enter an interger: '))
-# 	print('Your number:', my_num)
-#
-# except ValueError:
-# 	print('\n''That was not an Interger!')
+
+try:
+ 	my_num = int(input('Enter an interger: '))
+ 	print('Your number:', my_num)
+
+ except ValueError:
+ 	print('\n''That was not an Interger!')
 
 
 
